ec36d18888456c7f132e72e7bf1ca1dbca0e099f
Miguel TR <migueltr.2019@outlook.com>
2026-01-15 14:16:16 +0100

    ud3

---
+++ Unidad03/Ejemplo1/app.js
@@ -0,0 +1,16 @@
+"use strict";
+console.log("JS cargado ✅");
+
+const a = 10;
+const b = 3;
+
+console.log("Suma:", a + b);
+console.log("Resta:", a - b);
+console.log("Multiplica:", a * b);
+console.log("Divide:", a / b);
+console.log("Resto:", a % b);
+
+const nombre = "Compi";
+
+console.log("Hola " + nombre);
+console.log(`Hola ${nombre}`);
